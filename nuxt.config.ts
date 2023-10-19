523af6e09f107823c6b57b57d6e21131b644d537
Celia Chang <tina8899530@gmail.com>
2023-10-19 22:58:49 +0800

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+// nuxt.config.js
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  css: [
+    // SCSS file in the project
+    "~/assets/sass/main.sass", // you should add main.scss somewhere in your app
+  ],
+  nitro: {
+    preset: "firebase",
+    firebase: {
+      gen: 2,
+    },
+  },
+});
